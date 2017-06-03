c495bd0796286c60f0273e03165c1d28dfbef7f8
Your Name <tucholskimateusz@gmail.com>
2017-06-03 11:06:21 +0200

    testy klasy Calculator

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -1,10 +1,11 @@
 package test;
 
-import main.Calculator;
-
-import org.junit.Before;
 import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import main.Calculator;
 
 public class CalculatorTest {
 	
@@ -22,8 +23,70 @@ public class CalculatorTest {
 	}
 	
 	@Test
-	public void test1() throws Exception {
-		System.out.println("Metoda testowa");
+	public void testDodawania1() throws Exception {
+		double a = 1.2;
+		double b = 1.6;
+		
+		double wynik = classUnderTest.sum(a, b);
+		
+		Assert.assertEquals("B³êdny wynik dodawania", 2.8, wynik, 0.001);
+		
 	}
+	
+	@Test
+	public void testDodawania2() throws Exception {
+		double a = 3.2;
+		double b = 1.6;
+		
+		double wynik = classUnderTest.sum(a, b);
+		
+		Assert.assertEquals("B³êdny wynik dodawania", 4.8, wynik, 0.001);
+		
+	}
+	
+	@Test
+	public void testOdejmowania1() throws Exception {
+		double a = 3.3;
+		double b = 1.1;
+		
+		double wynik = classUnderTest.subtract(a, b);
+		
+		Assert.assertEquals("B³êdny wynik odejmowania", 2.2, wynik, 0.001);
 
+
+	}
+	
+	@Test
+	public void testMnozenia1() throws Exception {
+		double a = 3.3;
+		double b = 1.1;
+		
+		double wynik = classUnderTest.multiply(a, b);
+		
+		Assert.assertEquals("B³êdny wynik mno¿enia", 3.63, wynik, 0.001);
+
+
+	}
+	@Test
+	public void testDzielenia1() throws Exception {
+		double a = 3.3;
+		double b = 1.1;
+		
+		double wynik = classUnderTest.divide(a, b);
+		Assert.assertEquals("B³êdny wynik dzielenia", 3, wynik, 0.001);
+		
+
+
+	}@Test(expected = IllegalArgumentException.class)
+	public void testDzieleniaPrzezZero() throws Exception {
+		double a = 3.3;
+		double b = 0;
+		
+		classUnderTest.divide(a, b);
+		
+		
+
+
+	}
+	
 }
